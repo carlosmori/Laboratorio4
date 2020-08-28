216fc55784a2a9d6f988ba9cd6b38c8aaea04ca1
Carlos Mori <cmori.94@gmail.com>
2020-08-27 22:13:23 -0300

    feature: simple sum excercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demo';
+  edad1: number = null;
+  edad2: number = null;
+  promedio: number = 0
+  suma: number = 0;
+
+  Calcular(){
+    this.promedio = (+this.edad1 + +this.edad2) / 2;
+    this.suma = +this.edad1 + +this.edad2;
+  }
+  Refrescar(){
+    this.edad1 = null;
+    this.edad2 = null;
+    this.Calcular();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {WelcomeComponent} from '../components/welcome/welcome.component'
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
